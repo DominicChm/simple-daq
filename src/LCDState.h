adcffb083e1c404b2cc9c148949ddf427056527f
Joe Keenan <joekeenandev@gmail.com>
2022-05-13 20:14:39 -0700

    current code on the dac

--- src/LCDState.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Dominic on 3/22/2022.
-//
-
-#ifndef UNTITLED_LCDSTATE_H
-#define UNTITLED_LCDSTATE_H
-
-
-#endif //UNTITLED_LCDSTATE_H
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,12 @@
 #define PIN_SHOCK_BR A16
 #define PIN_SHOCK_FR A17
 
+#define PIN_HALL_FR A5 //Front right wheel
+#define PIN_HALL_FL A6 //Front left wheel
+#define PIN_HALL_REAR A7 //Rear wheels
+#define PIN_HALL_ESP1 A8 //Engine speed 1
+#define PIN_HALL_ESP2 A9 //Engine speed 2
+
 #define LCD_REFRESH_INTERVAL 250
 #define SENSOR_REFRESH_INTERVAL 33
 
@@ -23,6 +29,11 @@
 #define READ_RESTART() (*(volatile uint32_t *)RESTART_ADDR)
 #define WRITE_RESTART(val) ((*(volatile uint32_t *)RESTART_ADDR) = (val))
 
+void hall_fr_isr(void);
+void hall_fl_isr(void);
+void hall_rear_isr(void);
+void hall_esp1_isr(void);
+void hall_esp2_isr(void);
 
 struct {
     uint16_t BL;
@@ -31,11 +42,21 @@ struct {
     uint16_t FR;
 } shock_data;
 
-int NUM_SCREENS = 3;
+struct {
+    uint32_t FR;
+    uint32_t FL;
+    uint32_t REAR;
+    uint32_t ESP1;
+    uint32_t ESP2;
+} hall_data;
+
+int NUM_SCREENS = 5;
 enum LCDScreen {
     LOG_STATUS,
     ACCEL_MEASUREMENTS,
     SHOCKS,
+    HALL1,
+    HALL2,
 
     SD_ERROR,
     FILE_ERROR,
@@ -51,6 +72,12 @@ FsFile file;
 unsigned long t_start = 0;
 char filename[32];
 
+volatile uint32_t fr_revs;
+volatile uint32_t fl_revs;
+volatile uint32_t rear_revs;
+volatile uint32_t esp1_revs;
+volatile uint32_t esp2_revs;
+
 bool is_logging;
 
 DebouncedButton mode_switch(PIN_MODE_SWITCH, 500);
@@ -64,6 +91,18 @@ void setup() {
     pinMode(PIN_SHOCK_FL, INPUT);
     pinMode(PIN_SHOCK_BR, INPUT);
     pinMode(PIN_SHOCK_FR, INPUT);
+
+    pinMode(PIN_HALL_FR, INPUT);
+    pinMode(PIN_HALL_FL, INPUT);
+    pinMode(PIN_HALL_REAR, INPUT);
+    pinMode(PIN_HALL_ESP1, INPUT);
+    pinMode(PIN_HALL_ESP2, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(PIN_HALL_FR), hall_fr_isr, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_HALL_FL), hall_fl_isr, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_HALL_REAR), hall_rear_isr, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_HALL_ESP1), hall_esp1_isr, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_HALL_ESP2), hall_esp2_isr, RISING);
 
     lcd.begin(16, 2);
     lcd.print("DAQ Starting...");
@@ -81,6 +120,12 @@ void setup() {
 
         while (true);
     }
+
+    fr_revs = 0;
+    fl_revs = 0;
+    rear_revs = 0;
+    esp1_revs = 0;
+    esp2_revs = 0;
 
     Serial.println("MPU6050 Found!");
     mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
@@ -129,6 +174,18 @@ void render_LCD() {
             lcd.printf("BL:%d BR:%d", shock_data.BL, shock_data.BR);
             break;
 
+        case HALL1:
+            lcd.printf("FL:%d FR:%d", hall_data.FL, hall_data.REAR);
+            lcd.setCursor(0, 1);
+            lcd.printf("REAR:%d", hall_data.FR);
+            break;
+
+        case HALL2:
+            lcd.printf("ENGINE1:%d", hall_data.ESP1);
+            lcd.setCursor(0, 1);
+            lcd.printf("ENGINE2:%d", hall_data.ESP2);
+            break;
+
         case SD_ERROR:
             lcd.printf("SD INIT ERROR!");
             break;
@@ -167,13 +224,14 @@ void init_log() {
     }
 
     file.printf(
-            "Time (s), Xaccel (m/s^2), Yaccel (m/s^2), Zaccel (m/s^2), Xgyro (rad/s), Ygyro (rad/s), Zgyro (rad/s), FLsus, FRsus, BLsus, BRsus\n");
+            "Time (s), Xaccel (m/s^2), Yaccel (m/s^2), Zaccel (m/s^2), Xgyro (rad/s), Ygyro (rad/s), Zgyro (rad/s), FLsus, FRsus, BLsus, BRsus, FRONThall, REARhall, ENGINEhall\n");
     t_start = millis();
     lcd_screen = LOG_STATUS;
     is_logging = true;
 }
 
 void loop() {
+
     //INIT timers
     if (last_lcd_refresh == 0) last_lcd_refresh = millis();
     if (last_sensor_refresh == 0) last_sensor_refresh = millis();
@@ -212,13 +270,22 @@ void loop() {
         shock_data.BR = analogRead(PIN_SHOCK_BR);
         shock_data.FR = analogRead(PIN_SHOCK_FR);
 
+        hall_data.FR = fr_revs;
+        hall_data.FL = fl_revs;
+        hall_data.REAR = rear_revs;
+        hall_data.ESP1 = esp1_revs;
+        hall_data.ESP2 = esp2_revs;
+
 
         mpu.getEvent(&a, &g, &temp);
         //file.print("TEST TEST TEST");
-        file.printf("%f, %f, %f, %f, %f, %f, %f, %d, %d, %d, %d\n", (millis() - t_start) / 1000.f,
+        file.printf("%f, %f, %f, %f, %f, %f, %f, %d, %d, %d, %d, %d, %d, %d\n", (millis() - t_start) / 1000.f,
                     a.acceleration.x, a.acceleration.y, a.acceleration.z,
                     g.gyro.x, g.gyro.y, g.gyro.z,
-                    shock_data.FL, shock_data.FR, shock_data.BL, shock_data.BR);
+                    shock_data.FL, shock_data.FR, shock_data.BL, shock_data.BR,
+                    hall_data.REAR, hall_data.FR, hall_data.ESP1);
+
+        
 
 //
 //        /* Print out the values */
@@ -252,4 +319,24 @@ void loop() {
     }
 
 
+}
+
+void hall_fr_isr(void) {
+    fr_revs++;
+}
+
+void hall_fl_isr(void) {
+    fl_revs++;
+}
+
+void hall_rear_isr(void) {
+    rear_revs++;
+}
+
+void hall_esp1_isr(void) {
+    esp1_revs++;
+}
+
+void hall_esp2_isr(void) {
+    esp2_revs++;
 }
